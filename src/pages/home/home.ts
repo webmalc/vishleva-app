cf3e217e6e92f77fd2fdf19fa1645d646df2aac9
webmalc <m@webmalc.pw>
2017-04-15 13:51:20 +0300

    logs workflow

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -11,16 +11,28 @@ import { Settings } from '../../app/settings'
 export class HomePage {
 
   public settings: Settings
+  public logs: string[]
+  public logMax = 50
 
   constructor(
     public navCtrl: NavController,
     public settingsService: SettingsService
   ) {
     this.settings = new Settings()
+    this.logs = []
   }
-  ionViewWillEnter() {
+
+  ionViewWillEnter(): void {
     this.settings = this.getSettings()
+    setInterval(() => this.getSMS(), 3000)
   }
+
+  private getSMS(): string[] {
+    this.logs.push('item ' + new Date().toLocaleString())
+    this.logs = this.logs.slice(-this.logMax)
+    return this.logs
+  }
+
   getSettings(): Settings {
     return this.settingsService.get().then(val => this.settings = val)
   }
